424406a95b1d8631345090b4cfda743aeea1a941
Vinicius de Moraes <vimoraescarvalho@gmail.com>
2022-02-22 20:28:18 -0300

    adiciona include das dependencias

--- main.cpp
+++ main.cpp
@@ -3,6 +3,13 @@
 
 #include <glm/gtx/string_cast.hpp>
 
+#include <stb_image.h>
+
+#include <GL/glew.h>
+
+#include <GLFW/glfw3.h>
+
+
 int main() {
 
 	glm::vec3 V(1, 2, 3);
@@ -10,5 +17,10 @@ int main() {
 	// definindo a saída padrão
 	std::cout << "Boa noite, já sabemos C++" << std::endl;
 	std::cout << glm::to_string(V) << std::endl;
+
+	glfwInit();
+
+	glewInit();
+
 	return 0;
 }
